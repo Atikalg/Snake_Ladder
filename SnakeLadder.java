a7704091162bb1feb3f6c5da747f91b03d071f68
atikalg <who.atik@gmail.com>
2022-04-07 13:58:38 +0530

    Added : welcome to snake and ladder game

---
+++ SnakeLadder.java
@@ -0,0 +1,14 @@
+package com.bridgelab.Assignment4.SnakeLadder;
+
+/**
+ * @author atik
+ * Welcome message and start position is 0
+ */
+public class SnakeLadder {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to SNAKES AND LADDERS GAME!!");
+		
+		int startPositionA = 0;
+	}
+	}
